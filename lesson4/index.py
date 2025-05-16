dd393cc8e92ca8160a8257c144b5a33b39186213
My name is ming <victory2004_2_34@yahoo.com.tw>
2025-05-16 11:03:38 +0800

    s

--- lesson4/index.py
+++ lesson4/index.py
@@ -1,1 +1,13 @@
-print("hello flask")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<h1> Hello, World!</h1> 這是我的第一頁"
+@app.route("/user")
+def user():
+    return"<h1>user! </h1><p> 這是我的第2頁</p>"
+@app.route("/product")
+def product():
+    return"<h1>product! </h1><p> 這是我的第3頁</p>"
