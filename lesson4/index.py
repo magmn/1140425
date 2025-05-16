ec5fb2acde5b745c0be58bab398d1c77681aa6e0
My name is ming <victory2004_2_34@yahoo.com.tw>
2025-05-16 11:11:27 +0800

    2

--- lesson4/index.py
+++ lesson4/index.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1> Hello, World!</h1> 這是我的第一頁"
+    return render_template("index.html")
 @app.route("/user")
 def user():
     return"<h1>user! </h1><p> 這是我的第2頁</p>"
