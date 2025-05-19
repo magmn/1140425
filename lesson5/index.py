c54d8bfc0e161588b69ee62771a2f7773ff8ff63
My name is ming <victory2004_2_34@yahoo.com.tw>
2025-05-19 14:35:54 +0800

    1212232

---
+++ lesson5/index.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html.jinja2")
+
+@app.route("/classes")
+def classes():
+    return render_template("classes.html.jinja2")
+
+@app.route("/product")
+def product():
+    return "<h1>product!</h1><p>這是我的第3頁</p>"
