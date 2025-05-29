91cd744154df32a8872b11550e41ed56b76fb11a
My name is ming <victory2004_2_34@yahoo.com.tw>
2025-05-29 10:05:51 +0800

    4524

---
+++ lesson8/index.py
@@ -0,0 +1,44 @@
+from flask import Flask,render_template
+import os
+from dotenv import load_dotenv
+import psycopg2
+from psycopg2 import OperationalError
+
+# 載入 .env 檔案
+load_dotenv()
+conn_string = os.getenv('RENDER_DATABASE')
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html.jinja2")
+
+@app.route("/classes")
+def classes():
+    name = "Robert"
+    weekdays = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
+    return render_template("classes.html.jinja2",name=name,weekdays=weekdays)
+
+@app.route("/new")
+def new():
+    try:
+        conn = psycopg2.connect(conn_string)
+        raise Exception("出現錯誤")
+        print("連線成功")
+    except OperationalError as e:
+        print("連線失敗")
+        print(e)
+        return render_template("error.html.jinja2",error_message="資料庫錯誤"),500
+    except:
+        return render_template("error.html.jinja2",error_message="不知名錯誤"),500
+    conn.close()
+    return render_template("new.html.jinja2")
+
+@app.route("/traffic")
+def traffic():
+    return render_template("traffic.html.jinja2")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html.jinja2")
