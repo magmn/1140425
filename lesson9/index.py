91b7ec124f3209658a41f5f4712753850e123f52
My name is ming <victory2004_2_34@yahoo.com.tw>
2025-06-05 16:50:15 +0800

    1425

--- lesson9/index.py
+++ lesson9/index.py
@@ -6,7 +6,7 @@ from psycopg2 import OperationalError
 
 # 載入 .env 檔案
 load_dotenv()
-conn_string = os.getenv('RENDER_DATABA')
+conn_string = os.getenv('RENDER_DATABASE')
 
 app = Flask(__name__)
 
@@ -25,20 +25,21 @@ def new():
     try:
         conn = psycopg2.connect(conn_string)
         with conn.cursor() as cur:
-            sql = """SELECT * FROM 最新消息
-                    ORDER BY 上班日期 DESC;"""
+            sql = "SELECT * FROM 最新訊息"
+            sql = """SELECT * FROM public.最新訊息
+                     ORDER BY 上版日期 desc"""
             cur.execute(sql)
-        # 取得所有資料
+            # 取得所有資料
             rows = cur.fetchall()
-            print(rows)
-            
         
+        #raise Exception("出現錯誤")
+        #print("連線成功")
     except OperationalError as e:
         print("連線失敗")
         print(e)
-        return render_template("error.html.jinja2",error_message="資料庫錯誤"),500
+        return render_template('error.html.jinja2', error_message="資料庫錯誤"), 500
     except:
-        return render_template("error.html.jinja2",error_message="不知名錯誤"),500
+        return render_template('error.html.jinja2', error_message="不知名錯誤"), 500
     conn.close()
     return render_template("new.html.jinja2",rows=rows)
 
@@ -49,3 +50,4 @@ def traffic():
 @app.route("/contact")
 def contact():
     return render_template("contact.html.jinja2")
+
